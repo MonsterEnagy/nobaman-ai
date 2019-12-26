1452aa6525abd543f1506a20b5d5888481c300a2
Glitch (hello-webpage) <none>
2019-12-26 10:30:17 +0000

    🖐🌚 Checkpoint ./command/quiz.js:8778187/4701 ./package.json:8778187/1952 ./command/weather.js:8778187/1811

--- command/quiz.js
+++ command/quiz.js
@@ -1,56 +1,79 @@
-module.exports.run = async (client , message) => {
-    var result = [];
-      const parser = require('xml2json');
-   const request = require("request");
-    var shuffle = require('shuffle-array');
-    request({
-    url : "http://24th.jp/test/quiz/api_quiz.php",
-    method : "get",
-      xml : true
-    } , async (err , body) => {
-      if(err) throw new Error()
+module.exports.run = async (client, message) => {
+  var result = [];
+  const parser = require("xml2json");
+  const request = require("request");
+  const shuffle = require("array-shuffle");
+  request(
+    {
+      url: "http://24th.jp/test/quiz/api_quiz.php",
+      method: "get",
+      xml: true
+    },
+    async (err, body) => {
+      if (err) throw new Error();
 
       var body1 = parser.toJson(body.body);
-      var body = JSON.parse(body1)
-      var quiz = body.Result.quiz
-          var anser = quiz.ans1
-      var array = [quiz.ans1 , quiz.ans2 , quiz.ans3 , quiz.ans4];
+      var body = JSON.parse(body1);
+      var quiz = body.Result.quiz;
+      var anser = quiz.ans1;
+      var array = [quiz.ans1, quiz.ans2, quiz.ans3, quiz.ans4];
       shuffle(array);
       var result = [];
-      for(var i= 1,b = 0; i<=4; i++) {
-        result.push(`${i}. ${array[(i - 1)]}`)
+      for (var i = 1, b = 0; i <= 4; i++) {
+        result.push(`${i}. ${array[i - 1]}`);
       }
-const msg = await message.channel.send(quiz.quession +"\n"+ result.join("\n"))
-msg.react("1⃣")
-msg.react("2⃣")
-msg.react("3⃣")
-msg.react("4⃣")
- const filter = (reaction, user) => {
-    return ['4⃣', '3⃣' , "2⃣" , "1⃣"].includes(reaction.emoji.name) && user.id === message.author.id;
-};
+      const msg = await message.channel.send(
+        quiz.quession + "\n" + result.join("\n")
+      );
+      await msg.react("1⃣")
+      await msg.react("2⃣");
+      await msg.react("3⃣");
+      awaitmsg.react("4⃣");
+      const filter = (reaction, user) => {
+        return (
+          ["4⃣", "3⃣", "2⃣", "1⃣"].includes(reaction.emoji.name) &&
+          user.id === message.author.id
+        );
+      };
 
-      msg.awaitReactions(filter , {
-    max: 1,
-    time: 15 * 1000,
-   errors: ['time']
-  }).then(collected => {
-    const reaction = collected.first();
-    var anserbanngou1 = array.indexOf(anser);
-    var anserbanngou = String((anserbanngou1 + 1));
-    result.push(anserbanngou)
-  if(reaction.emoji.name === "1⃣" && anserbanngou === "1") {
-   return message.channel.send(`正解です！\n正解は、${anserbanngou}の${anser}でした！`)
-  }  else if(reaction.emoji.name === "2⃣" && anserbanngou === "2") {
-   return message.channel.send(`正解です！\n正解は、${anserbanngou}の${anser}でした！`)
-  } else if(reaction.emoji.name === "3⃣" && anserbanngou === "3") {
-       return message.channel.send(`正解です！\n正解は、${anserbanngou}の${anser}でした！`)
-  } else if(reaction.emoji.name==="4⃣" && anserbanngou === "4"){
-          return message.channel.send(`正解です！\n正解は、${anserbanngou}の${anser}でした！`)     
-            }
-             else{
-    message.channel.send(`違います！！！正解は**${anserbanngou}の${anser}**でした！`);
-  } 
-}).catch(err => message.channel.send(`時間切れです！正解は**${result}の${anser}**でした!`))
-      
-            })
-}
+      msg
+        .awaitReactions(filter, {
+          max: 1,
+          time: 15 * 1000,
+          errors: ["time"]
+        })
+        .then(collected => {
+          const reaction = collected.first();
+          var anserbanngou1 = array.indexOf(anser);
+          var anserbanngou = String(anserbanngou1 + 1);
+          result.push(anserbanngou);
+          if (reaction.emoji.name === "1⃣" && anserbanngou === "1") {
+            return message.channel.send(
+              `正解です！\n正解は、${anserbanngou}の${anser}でした！`
+            );
+          } else if (reaction.emoji.name === "2⃣" && anserbanngou === "2") {
+            return message.channel.send(
+              `正解です！\n正解は、${anserbanngou}の${anser}でした！`
+            );
+          } else if (reaction.emoji.name === "3⃣" && anserbanngou === "3") {
+            return message.channel.send(
+              `正解です！\n正解は、${anserbanngou}の${anser}でした！`
+            );
+          } else if (reaction.emoji.name === "4⃣" && anserbanngou === "4") {
+            return message.channel.send(
+              `正解です！\n正解は、${anserbanngou}の${anser}でした！`
+            );
+          } else {
+            message.channel.send(
+              `違います！！！正解は**${anserbanngou}の${anser}**でした！`
+            );
+          }
+        })
+        .catch(err =>
+          message.channel.send(
+            `時間切れです！正解は**${result}の${anser}**でした!`
+          )
+        );
+    }
+  );
+};
--- command/weather.js
+++ command/weather.js
@@ -1,21 +1,21 @@
-module.exports.run = (client , message ,kekka) => {
-  	        	const weather = require("weather-js");
-	        	const Discord = require("discord.js")
-        weather.find({search: kekka, degreeType: 'C'}, function(err, result) {
-        	if (err) message.channel.send(err);
-            if (result.length === 0) {
-                message.channel.send('**場所を取得できませんでした**') 
-                return; 
-            }            
-             var current = result[0].current;
-            const embed = new Discord.RichEmbed()
-                .setDescription('**' + current.skytext + '**') 
-                .setAuthor(`${current.date}の${current.observationpoint}の天気`) 
-                .setThumbnail(current.imageUrl)  
-                .addField('温度',`${current.temperature}℃`)
-                .addField('体感温度', `${current.feelslike}℃`)
-                .addField('風',current.winddisplay)
-                .addField('湿度', `${current.humidity}%`);
-            message.channel.send(embed);
-              });
-}
+module.exports.run = (client, message, kekka) => {
+  const weather = require("weather-js");
+  const Discord = require("discord.js");
+  weather.find({ search: kekka, degreeType: "C" }, function(err, result) {
+    if (err) message.channel.send(err);
+    if (result.length === 0) {
+      message.channel.send("**場所を取得できませんでした**");
+      return;
+    }
+    var current = result[0].current;
+    const embed = new Discord.RichEmbed()
+      .setDescription("**" + current.skytext + "**")
+      .setAuthor(`${current.date}の${current.observationpoint}の天気`)
+      .setThumbnail(current.imageUrl)
+      .addField("温度", `${current.temperature}℃`)
+      .addField("体感温度", `${current.feelslike}℃`)
+      .addField("風", current.winddisplay)
+      .addField("湿度", `${current.humidity}%`);
+    message.channel.send(embed);
+  });
+};
