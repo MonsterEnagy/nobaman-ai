8c1fa27206dc2f108836c83519b7d23f147ee8fe
Glitch (hello-webpage) <none>
2019-12-31 07:50:46 +0000

    🍢☄️ Checkpoint ./command/vc.js:8778187/706

--- command/vc.js
+++ command/vc.js
@@ -1,7 +1,6 @@
 var fs = require("fs");
 var Discord = require("discord.js");
-//  const VoiceText = require("voicetext");
-const voiceText = require('@shooontan/voicetext');
+const VoiceText = require("voicetext");
 module.exports.run = async (client, message , cooldown , yomiage) => {
 if (!message.member.voiceChannel) {
     message.channel.send("まずボイスチャンネルに入りましょう");
@@ -15,10 +14,8 @@ if (!message.member.voiceChannel) {
   cooldown.add(message.author.id);
   var now = message.createdTimestamp;
   
-  const apiKey = process.env.VTAPI;
-const vt = voiceText(apiKey);
-  //var voice = new VoiceText(process.env.VTAPI);
-/*  
+  var voice = new VoiceText(process.env.VTAPI);
+ 
   voice
     .speaker(voice.SPEAKER.SHOW)
     .emotion(voice.EMOTION.HAPPINESS)
@@ -28,31 +25,26 @@ const vt = voiceText(apiKey);
       if (e) {
         console.error(e);
         return;
-      }*/
-    const text = yomiage;
-  const speaker = 'show';
-  const bf1 = await vt.mp3(text, speaker); // return Buffer
-  const output1 = `${now}.mp3`;
-
-
-       fs.writeFile(output1, bf1, 'binary', e => {
+      }
+       fs.writeFile(`./${now}.wav`, buf, 'binary', async e => {
         if (e) {
           console.error(e);
           return;
         }
         const vc = message.member.voiceChannel;
-        vc.join().then(connection => {
-          if (e) {
-            console.error(e);
-            return;
-          }
+         if(message.guild.me.voiceChannel != vc) {
+        var connection = await vc.join()
+        } else {
+        var connection = await message.guild.me.voiceChannel.connection
+        }
+
           const dispatcher = connection.playFile(`./${now}.mp3`);
-          /*dispatcher.on("end", reason => {
+          dispatcher.on("end", reason => {
             fs.unlinkSync(`./${now}.mp3`, err => {
               if (err) console.log(err);
             });
-          });*/
+          });
         });
-      });
+       });
   cooldown.delete(message.author.id);
 };
