3095e42b994380625d0012c9c407f1640f54397a
Glitch (hello-webpage) <none>
2020-01-01 12:02:17 +0000

    ⏰🚣 Checkpoint ./command/omikuji.js:8778187/1892 ./database/db.json:8778187/6

--- command/omikuji.js
+++ command/omikuji.js
@@ -21,13 +21,14 @@ module.exports.run = (client , message, db , args) => {
         db.get("omikuji")
           .push({ id: message.author.id, time: `${y}/${m}/${d}`, nobaman: 0 , omikujinaiyou:"デフォルト"})
           .write();
+         db.set(`${message.guild.id}.omikuji` , [])
+          .push({ id: message.author.id, time: `${y}/${m}/${d}`, nobaman: 0 , omikujinaiyou:"デフォルト"})
+          .write();
       }
       var dbarray = db
         .get("omikuji")
         .find({ id: message.author.id })
         .value();
-      console.log(dbarray);
-      console.log(dbarray.time);
 
       /*
 大吉・・・約22％
@@ -62,6 +63,11 @@ module.exports.run = (client , message, db , args) => {
         })
           .assign({ omikujinaiyou: omikujikekka , nobaman: dbarray.nobaman + 1 })
           .write();
+                db.get(message.guild.id).get("omikuji").find({
+          id: message.author.id
+        })
+          .assign({ omikujinaiyou: omikujikekka , nobaman: dbarray.nobaman + 1 })
+          .write();
         return;
       }
       message.channel.send(`あなたは${omikujikekka}です。`);
@@ -71,6 +77,11 @@ module.exports.run = (client , message, db , args) => {
         .assign({ omikujinaiyou: omikujikekka })
         .write();
 
+            db.get(message.guild.id).get("omikuji").find({
+        id: message.author.id
+      })
+        .assign({ omikujinaiyou: omikujikekka })
+        .write();
     } else if (args[0] === "ranking") {
       const json = require("../database/db.json");
       const array = [];
@@ -87,6 +98,33 @@ const namearray = [];
       var name = [];
       let embed = new Discord.RichEmbed()
       .setTitle("のばまん吉獲得数ランキング")
+      .addField("1位:" +array[0][1], array[0][0])
+      .addField("2位:" +array[1][1], array[1][0])
+      .addField("3位:" +array[2][1], array[2][0])
+      .addField("4位:" +array[3][1], array[3][0])
+      .addField("5位:" +array[4][1], array[4][0])
+      .addField("6位:" +array[5][1], array[5][0])
+      .addField("7位:" +array[6][1], array[6][0])
+      .addField("8位:" +array[7][1], array[7][0])
+      .addField("9位:" +array[8][1], array[8][0])
+      .addField("10位:"+array[9][1], array[9][0])
+      message.channel.send(embed)
+    } else if(args[0] === "sabaran") {
+      const json = require("../database/db.json");
+      const array = [];
+      function compareFunc(a, b) {
+          return b[0] - a[0];
+      }
+      const namearray = [];
+      for (var i = 0; i < json.omikuji.length; i++) {
+        array.push([json[message.guild.id].omikuji[i].nobaman , client.users.get(json[message.guild.id].omikuji[i].id).username])
+      }
+      
+      array.sort(compareFunc);
+      
+      var name = [];
+      let embed = new Discord.RichEmbed()
+      .setTitle(`**${message.guild.name}**ののばまん吉獲得数ランキング`)
       .addField("1位:" +array[0][1], array[0][0])
       .addField("2位:" +array[1][1], array[1][0])
       .addField("3位:" +array[2][1], array[2][0])
