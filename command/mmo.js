67817d07a05986c0c01aeccde79f029b52b867ce
Glitch (hello-webpage) <none>
2020-01-02 06:58:27 +0000

    🖐😋 Checkpoint ./command/mmo.js:8778187/977

--- command/mmo.js
+++ command/mmo.js
@@ -4,6 +4,7 @@ db {
 mmo : [
   {
   "id" : "id",
+  "hp" : 10
   "strong" : "1"
   "teki" : "teki",
   "url" : "tekiのurl"
@@ -28,13 +29,25 @@ module.exports.run = (client , message, db , args) => {
       const tekidice = Math.floor( Math.random() * tekijson.length );
       const teki = tekijson[tekidice]
       json
-      .push({id : message.author.id , teki : teki.name, url : teki.url, strong:"1" , level: "1"})
-      
+      .push({id : message.author.id , teki : teki.name, url : teki.url, strong:1 , level: 1}).write()
+          const tekihp = json.level + 3
+    const tekistrong = json.strong - 6
       message.channel.send(Discord.RichEmbed()
                           .setTitle(`${teki.name}がやってきた！`)
+                           .addField("HP" , tekihp)
+                           .addField("攻撃力" , tekistrong)
                           .setImage(teki.url));
-    const tekihp = json.level + 3
-    const tekistrong = json.strong - 6
+      tekihp - json.strong
+      if(tekihp < 0) {
+      json
+      .assign({strong: json.strong + 1 , level: json.strong + 1 , hp : json.level + 5})
+      .remove({teki : teki.name , url:teki.url}).write()
+        return message.channel.send("倒しました！Lvがあがります!")
+      }
+      json.assign({hp : json.hp - tekistrong}).write()
+      message.channel.send(`攻撃しました！敵ののこりHPは${tekihp}です。`)
+      message.channel.send(`攻撃されました！あなたの残りHPは${json.hp}です。`)
+
     } else if(!json.teki){
       var strong = json.level + json.strong - Math.floor( Math.random() * 8 );
     } else {
