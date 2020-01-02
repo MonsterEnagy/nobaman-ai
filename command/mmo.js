38d5c42079f3221b9fc1f2793b7b826e11b41bbe
Glitch (hello-webpage) <none>
2020-01-02 07:34:26 +0000

    💁👏 Checkpoint ./command/mmo.js:8778187/7250

--- command/mmo.js
+++ command/mmo.js
@@ -1,4 +1,4 @@
-const Discord = require("discord.js")
+const Discord = require("discord.js");
 const low = require("lowdb");
 const FileSync = require("lowdb/adapters/FileSync");
 
@@ -25,52 +25,125 @@ mmo : [
 }
 
 */
-module.exports.run = (client , message, db , args) => {
-     var json = db
-      .get("mmo")
-      .find({ id: message.author.id })
+module.exports.run = (client, message, db, args) => {
+  var json = db.get("mmo").find({ id: message.author.id });
 
-     var tekijson = require("../database/teki.json");
-  if(args[0] === "attack") {
-console.log("通ってる")
-    if(!json.value()) {
-      console.log("通ってる")
-      const tekidice = Math.floor( Math.random() * tekijson.length );
-      const teki = tekijson.teki[tekidice]
-      console.log(tekidice)
-      json
-      .push({id : message.author.id , teki : teki.name, url : teki.url, strong:1 , level: 1}).write()
-
-      message.channel.send(Discord.RichEmbed()
-                          .setTitle(`${teki.name}がやってきた！`)
-                           .addField("HP" , json.value().tekihp)
-                           .addField("攻撃力" , json.value().tekistrong)
-                          .setImage(teki.url));
-      json.assign({tekihp : json.value().tekihp - json.strong}).write() //tekihp - json.strong
-      message.channel.send(`攻撃しました！敵ののこりHPは${json.value().tekihp}です。`)
-      if(json.value().tekihp < 0) {
-      json
-      .assign({strong: json.value().strong + 1 , level: json.value().level + 1 , hp : json.value().level + 5})
-      .remove({teki : teki.name , url:teki.url}).write()
-        return message.channel.send(`倒しました！Lvが${json.value().level}にあがります！`)
-      }
-      json.assign({hp : json.value().hp - json.value().tekistrong}).write()
-      
-      message.channel.send(`攻撃されました！あなたの残りHPは${json.hp}です。`)
-      
-      if(json.value().hp < 0) {
+  var tekijson = require("../database/teki.json");
+  if (args[0] === "attack") {
+    console.log("通ってる");
+    if (!json.value()) {
+      console.log("通ってる");
+      const tekidice = Math.floor(Math.random() * tekijson.teki.length);
+      const teki = tekijson.teki[tekidice];
+      console.log(tekidice);
+      db.get("mmo")
+        .push({
+          id: message.author.id,
+          teki: teki.name,
+          tekihp: 5 - 4,
+          url: teki.url,
+          strong: 1,
+          level: 1,
+          hp: 1 + 5
+        })
+        .write();
+      var json = db.get("mmo").find({ id: message.author.id });
+      console.log(json.value());
+      message.channel.send(
+        new Discord.RichEmbed()
+          .setTitle(`${teki.value().name}がやってきた！`)
+          .addField("HP", json.value().tekihp)
+          .addField("攻撃力", json.value().tekistrong)
+          .setImage(teki.url)
+      );
+      json.assign({ tekihp: json.value().tekihp - json.strong }).write(); //tekihp - json.strong
+      message.channel.send(
+        `攻撃しました！敵ののこりHPは${json.value().tekihp}です。`
+      );
+      if (json.value().tekihp < 0) {
         json
-      .assign({strong: json.value().strong - 1 , level: json.value().level - 1 , hp : json.value().level - 5})
-      .remove({teki : teki.name , url:teki.url , tekihp : json.value().tekihp , tekistrong : json.value().strong}).write()
-        return message.channel.send(`倒されてしまいました！！Lvが${json.value().level}に下がります！`)
+          .assign({
+            strong: json.value().strong + 1,
+            level: json.value().level + 1,
+            hp: json.value().level + 5
+          })
+          .remove({ teki: teki.name, url: teki.url })
+          .write();
+        return message.channel.send(
+          `倒しました！Lvが${json.value().level}にあがります！`
+        );
       }
+      json.assign({ hp: json.value().hp - json.value().tekistrong }).write();
 
-    } else if(!json.teki){
-      var strong = json.level + json.strong - Math.floor( Math.random() * 8 );
+      message.channel.send(
+        `攻撃されました！あなたの残りHPは${json.value().hp}です。`
+      );
+
+      if (json.value().hp < 0) {
+        json
+          .assign({
+            strong: json.value().strong - 1,
+            level: json.value().level - 1,
+            hp: json.value().level - 5
+          })
+          .remove({
+            teki: teki.name,
+            url: teki.url,
+            tekihp: json.value().tekihp,
+            tekistrong: json.value().strong
+          })
+          .write();
+        return message.channel.send(
+          `倒されてしまいました！！Lvが${json.value().level}に下がります！`
+        );
+      }
+    } else if (!json.teki) {
+      var strong = json.level + json.strong - Math.floor(Math.random() * 8);
     } else {
-      var strong = json.level + json.strong - Math.floor( Math.random() * 8 );
-      tekidb.get("teki").find({ name : json.value().teki})
+      var strong = json.level + json.strong - Math.floor(Math.random() * 8);
+      tekidb.get("teki").find({ name: json.value().teki });
+            json.assign({ tekihp: json.value().tekihp - json.strong }).write(); //tekihp - json.strong
+      message.channel.send(
+        `攻撃しました！敵ののこりHPは${json.value().tekihp}です。`
+      );
+      if (json.value().tekihp < 0) {
+        json
+          .assign({
+            strong: json.value().strong + 1,
+            level: json.value().level + 1,
+            hp: json.value().level + 5
+          })
+          .remove({ teki: teki.name, url: teki.url })
+          .write();
+        return message.channel.send(
+          `倒しました！Lvが${json.value().level}にあがります！`
+        );
+      }
+      json.assign({ hp: json.value().hp - json.value().tekistrong }).write();
+
+      message.channel.send(
+        `攻撃されました！あなたの残りHPは${json.value().hp}です。`
+      );
+
+      if (json.value().hp < 0) {
+        json
+          .assign({
+            strong: json.value().strong - 1,
+            level: json.value().level - 1,
+            hp: json.value().level - 5
+          })
+          .remove({
+            teki: teki.name,
+            url: teki.url,
+            tekihp: json.value().tekihp,
+            tekistrong: json.value().strong
+          })
+          .write();
+        return message.channel.send(
+          `倒されてしまいました！！Lvが${json.value().level}に下がります！`
+        );
+      }
     }
   }
-  db.write()
-}
+  db.write();
+};
