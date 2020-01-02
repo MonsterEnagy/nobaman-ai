c56885be5e2e8a86c461625de35cb5c4a8f83724
Glitch (hello-webpage) <none>
2020-01-02 06:18:26 +0000

    🦀🖼 Checkpoint ./command/mmo.js:8778187/973

--- command/mmo.js
+++ command/mmo.js
@@ -4,7 +4,10 @@ db {
 mmo : [
   {
   "id" : "id",
-  "teki" : "teki"
+  "strong" : "1"
+  "teki" : "teki",
+  "url" :
+  "level" : "1"
   },
 {
 
@@ -19,12 +22,21 @@ module.exports.run = (client , message, db , args) => {
       .find({ id: message.author.id })
      var tekijson = require("../database/teki.json");
   if(args[0] === "attack") {
+
     if(!json) {
-      const teki = 
-      json
-      .push({})
-    } else {
       
+      const tekidice = Math.floor( Math.random() * tekijson.length );
+      const teki = tekijson[tekidice]
+      json
+      .push({id : message.author.id , teki : teki.name, url : teki.url, strong:"1" , level: "1"})
+      
+      message.channel.send(Discord.RichEmbed()
+                          .setTitle(`${teki.name}がやってきた！`)
+                          .setImage(teki.url));
+    } else if(!json.teki){
+      var strong = json.level + json.strong - Math.floor( Math.random() * 8 );
+    } else {
+      var strong = json.level + json.strong - Math.floor( Math.random() * 8 );
     }
   }
   db.write()
