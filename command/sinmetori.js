0ccc8b0beddf5a0e820ebc43837fc2e6bb65e231
Glitch (hello-webpage) <none>
2020-01-08 09:00:18 +0000

    🛎🏦 Checkpoint ./command/sinmetori.js:8778187/443

--- command/sinmetori.js
+++ command/sinmetori.js
@@ -42,12 +42,13 @@ module.exports.run = (client , message ,args) => {
     if(args[1] === "1") {
     img.onload = function(){    
     var canvas = Canvas.createCanvas(img.width, img.height);
+    //  canvas.setAttribute('height', img.height);
     var ctx = canvas.getContext('2d');
     ctx.drawImage(img, 0, 0, img.width, img.height);
     const w = img.width
     const h = img.height
     const rate = 0.5
-                    
+                                    
                     ctx.drawImage(img, 0, 0, w * rate, h, 0, 0, w * rate, h);
                     ctx.scale(-1, 1);
                     ctx.drawImage(img, 0, 0, w * rate, h, -w * (rate * 2), 0, w * rate, h);
@@ -64,7 +65,7 @@ module.exports.run = (client , message ,args) => {
     const w = img.width
     const h = img.height
     const rate = 0.5
-                    
+                    // canvas.setAttribute('width', w * rate * 2);
                     ctx.drawImage(img, w * rate, 0, w * (1 - rate), h, w * (1 - rate), 0, w * (1 - rate), h);
                     ctx.scale(-1, 1);
                     ctx.drawImage(img, w * rate, 0, w * (1 - rate), h, -w * (1 - rate), 0, w * (1 - rate), h);
