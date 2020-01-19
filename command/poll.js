cefc5e12199e7586defecf4f8cc02f405f529b4e
Glitch (hello-webpage) <none>
2020-01-19 07:51:48 +0000

    🚖☎️ Checkpoint ./command/poll.js:8778187/372 ./database/nobaman.json:8778187/7

--- command/poll.js
+++ command/poll.js
@@ -1,18 +1,20 @@
 const Discord = require("discord.js")
-module.exports.run = (client , message,  args) => {
+module.exports.run = async (client , message,  args) => {
   if(args.length > 21) return message.channel.send("20個までです")
   const index = [];
   for(var i= 0; args.length > i; i++) {
-    index.push(args[i])
+    const emoji = " 🇦 🇧 🇨 🇩 🇪 🇫 🇬 🇭 🇮 🇯 🇰 🇱 🇲 🇳 🇴 🇵 🇶 🇷 🇸 🇹 🇺 🇻 🇼 🇽 🇾 🇿".split(" ")
+
+    index.push(emoji[i] + args[i])
   }
   let embed = new Discord.RichEmbed()
   .setTitle(index[0])
   .setDescription(index.slice(1).join("\n"))
 message.channel.send(embed)
-  .then(msg => {
-  const emoji = "🇦 🇧 🇨 🇩 🇪 🇫 🇬 🇭 🇮 🇯 🇰 🇱 🇲 🇳 🇴 🇵 🇶 🇷 🇸 🇹 🇺 🇻 🇼 🇽 🇾 🇿".split(" ")
-  for(var i= 1; index > i; i++) {
-    msg.react(emoji[i])
+  .then(async msg => {
+  const emoji = " 🇦 🇧 🇨 🇩 🇪 🇫 🇬 🇭 🇮 🇯 🇰 🇱 🇲 🇳 🇴 🇵 🇶 🇷 🇸 🇹 🇺 🇻 🇼 🇽 🇾 🇿".split(" ")
+  for(var i= 1; index.length > i; i++) {
+    await msg.react(emoji[i])
   }
 })
 }
