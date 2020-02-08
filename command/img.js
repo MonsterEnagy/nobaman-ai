2e531390a71c95bcfa93809bc0ac68f5f2da03e2
Glitch (hello-webpage) <none>
2020-02-08 14:05:00 +0000

    🌗👨‍👨‍👦 Checkpoint ./command/img.js:8778187/57

--- command/img.js
+++ command/img.js
@@ -13,8 +13,8 @@ const result = await customSearch.cse.list({
   num: 1, // max:10
   start:  1,
 })
-  JSON.parse(result)
   console.log(result.data.items[0])
   let embed = new Discord.RichEmbed()
   .setImage(result.data.items[0].link)
+  message.channel.send(embed)
 }
