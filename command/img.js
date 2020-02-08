e2f7bd2f06bc5a4a04a192ff779e883d3829f6a2
Glitch (hello-webpage) <none>
2020-02-08 13:29:44 +0000

    🍝🏮 Checkpoint ./command/img.js:8778187/595 ./package.json:8778187/1787

--- command/img.js
+++ command/img.js
@@ -1,14 +1,19 @@
-module.exports.run = (client , message , kekka) => {
+module.exports.run = async (client , message , kekka) => {
   const Discord = require("discord.js")
-  const gis = require("g-i-s");
-  gis(kekka , logResults);
+ 
+  const { google } = require('googleapis');
+const customSearch = google.customsearch('v1');
 
-function logResults(error, results) {
-  if (error) {
-    console.log(error);
-  }
-  else {
-    if(results.length === 0) return message.channel.send("画像が見つかりませんでした")
+const result = await customSearch.cse.list({
+  cx: CSE_ID,
+  q: kekka,
+  auth: "AIzaSyAG0L1cTYEh-5f7puZgGGdp_XxIWBKqATE",
+  searchType: 'image',
+  safe: 'high',
+  num: searchNum, // max:10
+  start: startIndex + 1,
+});
+  if(results.length === 0) return message.channel.send("画像が見つかりませんでした")
     let embed = new Discord.RichEmbed()
     .setImage(results[0].url)
    message.channel.send(embed)
