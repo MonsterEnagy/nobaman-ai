c27862a2d7ac71a4119a539833fd6349a875ca2c
Glitch (nobaman-ai) <none>
2020-02-28 09:58:31 +0000

    🎡🎩 Checkpoint ./command/niconicodaihyakka.js:8778187/9076 ./package.json:8778187/1967

--- command/niconicodaihyakka.js
+++ command/niconicodaihyakka.js
@@ -1,20 +1,22 @@
 const rp = require('request-promise');
 const cheerio = require('cheerio');
 module.exports.run = async (client,message,kekka)=> {
-const options = {
-  transform: (body) => {
-    return cheerio.load(body);
-  }
-};
-rp.get('https://dic.nicovideo.jp/a/' + encodeURIComponent(kekka),  options)
-  .then(($) => {
-  console.log($("title").text())
-  //  return $('title').text();
-  })/*.then((title) => {
+const url = "www.wikihouse.com/taiko/index.php?cmd=read&page=%C6%F1%B0%D7%C5%D9%C9%BD%2F%A4%AA%A4%CB%2F%A5%BF%A5%A4%A5%B3%A5%ED%A1%BC%A5%EB&word=%A5%BF%A5%A4%A5%B3%A5%ED%A1%BC%A5%EB"
+(async () => {
+    // httpリクエストを投げる
+    const response = await requestPromise(
+      uri: url,
+      gzip: true
+    });
+    let $ = cheerio.load(response);
+
+    // 取得したいデータのタグを指定する
+    let title = $('.a-section .a-size-medium.a-color-base.a-text-normal').text();
     console.log(title);
-  }).catch((error) => {
-    console.error('Error:', error);
-  });*/
+
+    return title;
+  };
+})();
 
 
 }
