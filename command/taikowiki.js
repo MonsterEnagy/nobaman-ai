5beb3b4d5502fa052bb69abcc6e740c43e2a4b40
Glitch (nobaman-ai) <none>
2020-02-28 10:21:38 +0000

    👍🎎 Checkpoint ./command/taikowiki.js:8778187/249 ./package.json:8778187/2023

--- command/taikowiki.js
+++ command/taikowiki.js
@@ -1,18 +1,22 @@
 const rp = require('request-promise');
 const cheerio = require('cheerio');
+const Iconv = require("iconv").Iconv
+var iconv = new Iconv('UTF-8', 'ASCII');
 module.exports.run = async (client,message,kekka)=> {
 const url = "http://www.wikihouse.com/taiko/index.php?cmd=read&page=%C6%F1%B0%D7%C5%D9%C9%BD%2F%A4%AA%A4%CB%2F%A5%BF%A5%A4%A5%B3%A5%ED%A1%BC%A5%EB&word=%A5%BF%A5%A4%A5%B3%A5%ED%A1%BC%A5%EB"
 
     // httpリクエストを投げる
     const response = await rp({
       "uri": url,
-      gzip: true 
+      gzip: true,
+      encoding: null
     });
     let $ = cheerio.load(response);
     console.log("yeah")
     // 取得したいデータのタグを指定する
-    let title = $('list1' , 'ul').text();
-    console.log(title);
+    let title = $('ul' , '.list1').text();
+    const stringData = iconv.convert(title, "utf-8");
+    message.channel.send(title);
 
     return title;
 
