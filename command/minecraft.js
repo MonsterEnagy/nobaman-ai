150bf1d3d2bc84a290b7f65c3f45fde1f8bf76a9
Glitch (nobaman-ai) <none>
2020-02-28 12:01:24 +0000

    🏵💓 Checkpoint ./command/minecraft.js:8778187/788

--- command/minecraft.js
+++ command/minecraft.js
@@ -1,3 +1,4 @@
+const Discord = require("discord.js")
 module.exports.run = (client,message,args) => {
   const option = {
     "url" : `https://api.mojang.com/users/profiles/minecraft/${args[0]}`,
@@ -7,7 +8,20 @@ module.exports.run = (client,message,args) => {
   const request = require("request")
   request(option , (error , response , body) => {
     if(response)
-      if()
+      if(response.statusCode == 204) return message.channel.send("プレイヤーが見つかりませんでした")
+    const uuid = body.id
+    const name = body.name
+    request({
+      "url" : `https://sessionserver.mojang.com/session/minecraft/profile/${uuid}`,
+      "method" : "get",
+      json : true
+    } , (error , response , body) => {
+      console.log(body)
+      let embed = new Discord.RichEmbed()
+      .setTitle(`${name}の情報`)
+      .setImage(Buffer.alloc(body.properties[0].value,'base64'))
+      message.channel.send(embed)
+    })
     console.log(body)
   })
 }
