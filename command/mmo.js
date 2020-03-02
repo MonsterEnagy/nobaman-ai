55cbcc73842e084b2c8c818c74b5154ed863f029
Glitch (nobaman-ai) <none>
2020-03-02 10:54:31 +0000

    👯🐢 Checkpoint ./database/db.json:8778187/3698 ./command/mmo.js:8778187/14268

--- command/mmo.js
+++ command/mmo.js
@@ -18,8 +18,8 @@ function teki(message , json){
       message.channel.send(
         new Discord.RichEmbed()
           .setTitle(`${teki.name}がやってきた！`)
-          .addField("HP", json.value().tekihp)
-          .addField("攻撃力", json.value().strong)
+          .addField("HP",1 * json.value().level)
+          .addField("攻撃力", 1 * json.value().level)
           .setImage(teki.url)
       );
 }
@@ -92,39 +92,402 @@ module.exports.run = async (client, message, db, args) => {
 
   var tekijson = require("../database/teki.json");
   if (args[0] === "attack") {
-if(!json.value()) { 
+    //console.log("通ってる");
+    if (!json.value()) {
+      // console.log("通ってる");
+      const tekidice = Math.floor(Math.random() * tekijson.teki.length);
+      const teki = tekijson.teki[tekidice];
+      // console.log(tekidice);
       db.get("mmo")
         .push({
-          id: null,
-          teki: null,
-          tekihp: 1,
-          url: null,
+          id: message.author.id,
+          teki: teki.name,
+          tekihp: 5 - 4,
+          url: teki.url,
           strong: 10,
           level: 1,
-          hp: 10
+          hp: 1 + 5
         })
         .write();
-  message.channel.send("登録完了")
-} 
-  teki(message , json)
-    const strong = 10 * json.value().level
-    const tekistrong = 1 * json.value().level
+      var json = db.get("mmo").find({ id: message.author.id });
+      var strong = 15;
+      var tekistrong = 10;
+      //  console.log(json.value());
+      message.channel.send(
+        new Discord.RichEmbed()
+          .setTitle(`${teki.name}がやってきた！`)
+          .addField("HP", json.value().tekihp)
+          .addField("攻撃力", json.value().strong)
+          .setImage(teki.url)
+      );
+      json.assign({ tekihp: json.value().tekihp - strong }).write(); //tekihp - json.strong
 
-json.assign({ tekihp: json.value().tekihp - strong }).write();
-  if(tekijudge(message , json)) return;
-         json.assign({ hp: json.value().hp - tekistrong }).write();
-    if(judge(message , json)) return;
- /*while(1){
-        json.assign({ hp: json.value().hp - tekistrong}).write();
-    if(judge(message , json)) {
-      break;
-        
-    }
-        json.assign({ tekihp: json.value().tekihp - strong }).write();
-  if(tekijudge(message , json)) {
-    break;
+      if (json.value().tekihp < 0) {
+        json
+          .assign({
+            strong: json.value().strong + 1,
+            level: json.value().level + 1,
+            hp: json.value().level + 5
+          })
+          .remove({ teki: teki.name, url: teki.url })
+          .write();
+        return message.channel.send(
+          `攻撃しました！敵ののこりHPは${
+            json.value().tekihp
+          }です。\n倒しました！Lvが${json.value().level}にあがります！`
+        );
       }
-  }*/
+      json.assign({ hp: json.value().hp - tekistrong }).write();
+
+      message.channel.send(
+        `攻撃されました！あなたの残りHPは${json.value().hp}です。`
+      );
+
+      if (json.value().hp < 0) {
+        json
+          .assign({
+            strong: json.value().strong - 1,
+            level: json.value().level - 1,
+            hp: json.value().level - 5
+          })
+          .remove({
+            teki: teki.name,
+            url: teki.url,
+            tekihp: json.value().tekihp
+          })
+          .write();
+        return message.channel.send(
+          `攻撃されました！あなたの残りHPは${
+            json.value().hp
+          }です。\n倒されてしまいました！！Lvが${
+            json.value().level
+          }に下がります！`
+        );
+      }
+      message.channel.send(
+        `攻撃しました！敵ののこりHPは${
+          json.value().tekihp
+        }です。\n攻撃されました！あなたの残りHPは${json.value().hp}です。`
+      );
+    } else if (json.value().death === true || json.value().tekideath === true) {
+      //------------------------------------------------------------------------------------------------------------------------------------
+      // console.log("!json.tekiでーす");
+        var strong = json.value().hp + json.value().level
+  var tekistrong = Math.floor(json.value().hp / json.value().level) + 3;
+      const tekidice = Math.floor(Math.random() * tekijson.teki.length);
+      const teki = tekijson.teki[tekidice];
+      var json = db.get("mmo").find({ id: message.author.id });
+      json
+        .assign({
+          teki: teki.name,
+          url: teki.url,
+          tekihp:
+            Math.floor(json.value().hp / 1.001) +
+            Math.floor(json.value().strong / 1.001),
+          tekideath: false,
+          death: false
+        })
+        .write();
+      // console.log(json.value());
+      await message.channel.send(
+        new Discord.RichEmbed()
+          .setTitle(`${teki.name}がやってきた！`)
+          .addField("HP", json.value().tekihp)
+          .addField("攻撃力", json.value().strong - 4)
+          .setImage(teki.url)
+      );
+      /*     if(json.value().level < 300) {
+       var strong =
+        json.value().level +
+        json.value().strong -
+        Math.floor(Math.random() * 15);
+      var tekistrong =
+        json.value().level +
+        json.value().strong -40 -
+        Math.floor(Math.random() * 100);
+      } else if(json.value().level < 470){
+       var strong =
+        json.value().level +
+        json.value().strong -
+        Math.floor(Math.random() * 250);
+      var tekistrong =
+        json.value().level +
+        json.value().strong -200 -
+        Math.floor(Math.random() * 500);
+      } else if(json.value().level > 650) {
+             var strong =
+        json.value().level +
+        json.value().strong -
+        Math.floor(Math.random() * 320);
+      var tekistrong =
+        json.value().level +
+        json.value().strong -25 -
+        Math.floor(Math.random() * 650);
+                } else {
+       var strong =
+        json.value().level +
+        json.value().strong -
+        Math.floor(Math.random() * 200);
+      var tekistrong =
+        json.value().level +
+        json.value().strong -50 -
+        Math.floor(Math.random() * 500);
+      } */
+
+       if (json.value().level < 100) {
+        message.channel.send(`${message.author.username}はメラを放った！`);
+        var strong = Math.floor(strong * 12);
+      } else if (json.value().level < 200) {
+        message.channel.send(`${message.author.username}はメラミを放った！`);
+        var strong = (await strong) * 2;
+      } else if (json.value().level < 300) {
+        message.channel.send(
+          `${message.author.username}はメラゾーマを放った！`
+        );
+        var strong = (await strong) * 3;
+      } else if (json.value().level < 400) {
+        message.channel.send(`${message.author.username}はギラを放った！`);
+        var strong = (await strong) * 4;
+      } else if (json.value().level < 500) {
+        message.channel.send(`${message.author.username}はベギラマを放った！`);
+        var strong = (await strong) * 5;
+      } else if (json.value().level < 600) {
+        message.channel.send(`${message.author.username}はイオを放った！`);
+        var strong = (await strong) * 6;
+      } else if (json.value().level < 700) {
+        message.channel.send(`${message.author.username}はイオラを放った！`);
+        var strong = (await strong) * 7;
+      } else if (json.value().level < 800) {
+        message.channel.send(
+          `${message.author.username}はイオナズンを放った！`
+        );
+        var strong = (await strong) * 8;
+      } else if (json.value().level < 900) {
+        message.channel.send(
+          `${message.author.username}はヒャダルコを放った！`
+        );
+        var strong = (await strong) * 9;
+      } else if (json.value().level < 1000) {
+        message.channel.send(
+          `${message.author.username}はヒャダインを放った！`
+        );
+        var strong = (await strong) * 10;
+      } else if (json.value().level < 2000) {
+        message.channel.send(
+          `${message.author.username}はマヒャダインを放った！`
+        );
+        var strong = (await strong) * 11;
+      } else {
+        message.channel.send(`${message.author.username}は最後の歌を歌った！`);
+        var strong = (await strong) * 12;
+      }
+
+      if (Math.floor(Math.random() * 50) === 1) {
+        var strong = strong * 83412874121;
+        message.channel.send("会心の一撃！");
+      }
+      json.assign({ tekihp: json.value().tekihp - strong }).write(); //tekihp - json.strong
+
+      if(json.value().tekihp < 0) {
+      json
+        .assign({
+          strong: json.value().strong * 2,
+          level: json.value().level + 10,
+          hp: json.value().hp * 2,
+          tekideath: true
+        })
+
+        .remove({ teki: teki.name, url: teki.url })
+        .write();
+      return message.channel.send(
+        `攻撃しました！ダメージ${strong}敵ののこりHPは${
+          json.value().tekihp
+        }です。\n倒しました！Lvが${json.value().level}にあがります！`
+      );
+    }
+
+      json.assign({ hp: json.value().hp - tekistrong }).write();
+
+      if (json.value().hp < 0) {
+        json
+          .assign({
+            strong: json.value().strong,
+            level: json.value().level - 1,
+            hp: json.value().hp * 2,
+            death: true
+          })
+          .remove({
+            teki: teki.name,
+            url: teki.url,
+            tekihp: json.value().tekihp
+          })
+          .write();
+        return message.channel.send(
+          `攻撃されました！${tekistrong}あなたの残りHPは${
+            json.value().hp
+          }です。\n倒されてしまいました！！Lvが${
+            json.value().level
+          }に下がります！`
+        );
+      }
+      message.channel.send(
+        `攻撃しました！ダメージ${strong}敵ののこりHPは${
+          json.value().tekihp
+        }です。\n攻撃されました！${tekistrong}あなたの残りHPは${
+          json.value().hp
+        }です。`
+      );
+    } else {
+      //------------------------------------------------------------------------------------------------------------------------------------
+      // console.log("else通りでーす！");
+      /*   if(json.value().level < 300) {
+       var strong =
+        json.value().level +
+        json.value().strong -
+        Math.floor(Math.random() * 15);
+      var tekistrong =
+        json.value().level +
+        json.value().strong -40 -
+        Math.floor(Math.random() * 100);
+      } else if(json.value().level < 470){
+       var strong =
+        json.value().level +
+        json.value().strong -
+        Math.floor(Math.random() * 250);
+      var tekistrong =
+        json.value().level +
+        json.value().strong -2 -
+        Math.floor(Math.random() * 30);
+      } else if(json.value().level > 650) {
+             var strong =
+        json.value().level +
+        json.value().strong -
+        Math.floor(Math.random() * 320);
+      var tekistrong =
+        json.value().level +
+        json.value().strong -205 -
+        Math.floor(Math.random() * 950);
+                } else {
+       var strong =
+        json.value().level +
+        json.value().strong -
+        Math.floor(Math.random() * 200);
+      var tekistrong =
+        json.value().level +
+        json.value().strong -50 -
+        Math.floor(Math.random() * 500);
+      }
+          if(args[1]) {*/
+  var strong = json.value().hp + json.value().level
+  var tekistrong = Math.floor(json.value().hp / json.value().level) + 9;
+      if (json.value().level < 100) {
+        message.channel.send(`${message.author.username}はメラを放った！`);
+        var strong = Math.floor(strong * 12);
+      } else if (json.value().level < 200) {
+        message.channel.send(`${message.author.username}はメラミを放った！`);
+        var strong = (await strong) * 2;
+      } else if (json.value().level < 300) {
+        message.channel.send(
+          `${message.author.username}はメラゾーマを放った！`
+        );
+        var strong = (await strong) * 323;
+      } else if (json.value().level < 400) {
+        message.channel.send(`${message.author.username}はギラを放った！`);
+        var strong = (await strong) * 34234;
+      } else if (json.value().level < 500) {
+        message.channel.send(`${message.author.username}はベギラマを放った！`);
+        var strong = (await strong) * 52344234;
+      } else if (json.value().level < 600) {
+        message.channel.send(`${message.author.username}はイオを放った！`);
+        var strong = (await strong) * 61442342432;
+      } else if (json.value().level < 700) {
+        message.channel.send(`${message.author.username}はイオラを放った！`);
+        var strong = (await strong) * 411323424427;
+      } else if (json.value().level < 800) {
+        message.channel.send(
+          `${message.author.username}はイオナズンを放った！`
+        );
+        var strong = (await strong) * 3221123133418;
+      } else if (json.value().level < 900) {
+        message.channel.send(
+          `${message.author.username}はヒャダルコを放った！`
+        );
+        var strong = (await strong) * 121231313421339;
+      } else if (json.value().level < 1000) {
+        message.channel.send(
+          `${message.author.username}はヒャダインを放った！`
+        );
+        var strong = (await strong) * 1121321313134320;
+      } else if (json.value().level < 2000) {
+        message.channel.send(
+          `${message.author.username}はマヒャダインを放った！`
+        );
+        var strong = (await strong) * 11132;
+      } else {
+        message.channel.send(`${message.author.username}は最後の歌を歌った！`);
+        var strong = (await strong) * 1221787873;
+      }
+
+      if (Math.floor(Math.random() * 50) === 1) {
+        var strong = strong * 2;
+        message.channel.send("会心の一撃！");
+      }
+      json.assign({ tekihp: json.value().tekihp - strong }).write(); //tekihp - json.strong
+      // console.log(json.value());
+      if(json.value().tekihp < 0) {
+      json
+        .assign({
+          strong: json.value().strong * 2,
+          level: json.value().level + 1,
+          hp: json.value().hp * 2,
+          tekideath: true
+        })
+        .remove({
+          teki: json.value().teki,
+          url: json.value().url,
+          tekihp: json.value().tekihp
+        })
+        .write();
+      return message.channel.send(
+        `攻撃しました！ダメージ${strong}　敵ののこりHPは${
+          json.value().tekihp
+        }です。\n倒しました！Lvが${json.value().level}にあがります！`
+      );
+      }
+
+      json.assign({ hp: json.value().hp - tekistrong }).write();
+
+      if (json.value().hp < 0) {
+        json
+          .assign({
+            strong: json.value().strong * 2,
+            level: json.value().level - 1,
+            hp: json.value().hp * 2,
+            death: true
+          })
+          .remove({
+            teki: json.value().teki,
+            url: json.value().url,
+            tekihp: json.value().tekihp
+          })
+          .write();
+        return message.channel.send(
+          `攻撃されました！ダメージ${tekistrong}　あなたの残りHPは${
+            json.value().hp
+          }です。\n倒されてしまいました！！Lvが${
+            json.value().level
+          }に下がります！`
+        );
+      }
+
+      message.channel.send(
+        `攻撃しました！ダメージ${strong}　敵ののこりHPは${
+          json.value().tekihp
+        }です。\n攻撃されました！ダメージ${tekistrong}　あなたの残りHPは${
+          json.value().hp
+        }です。`
+      );
+    }
   } else if (args[0] === "status") {
     if (!json)
       return message.channel.send("一回プレイしてからステータスを見てね");
