1d882a15809896e9ec0b81e50bdd11555ecd0f71
Glitch (nobaman-ai) <none>
2020-03-03 10:30:05 +0000

    🎹☎️ Checkpoint ./command/emojicreate.js:8778187/49

--- command/emojicreate.js
+++ command/emojicreate.js
@@ -6,19 +6,19 @@ module.exports.run = (client, message, kekka) => {
 
  var setting = setting || {
     text: kekka,
-    color: '#000',
+    color: '#FFFFFF',
     fontFamily: 'ComicSans'
   }
 
   const text_n = setting.text
 
-  const c = Canvas.createCanvas(128, 128)
+  const c = Canvas.createCanvas(1280 , 820)
   const ctx = c.getContext('2d')
 
   ctx.font = 'bold 45px ' + setting.fontFamily
   ctx.textAlign = 'center'
   ctx.fillStyle = setting.color
-  ctx.fillText(text_n, 64, 56)
+  ctx.fillText(text_n, 1280, 56)
 
     const attachment = new Discord.Attachment(c.toBuffer(), "Emoji.png");
 
