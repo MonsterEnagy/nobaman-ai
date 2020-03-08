e9f47c576c790bc982d4212d7425af86f9a9cecd
Glitch (nobaman-ai) <none>
2020-03-08 08:58:15 +0000

    👕🍅 Checkpoint ./command/osero.js:8778187/1506

--- command/osero.js
+++ command/osero.js
@@ -19,14 +19,14 @@ module.exports.run = (client , message , db , args) => {
   if(!json) {
     var playBoard = [
       [":black_large_square:",":one:",":two:",":four:",":five:",":six:",":seven:",":eight:"]
-      [":one:",0,0,0,0,0,0,0,0],
-      [":two:",0,0,0,0,0,0,0,0],
-      [":three:",0,0,0,0,0,0,0,0],
-      [":four:",0,0,0,1,2,0,0,0],
-      [":five:",0,0,0,2,1,0,0,0],
-      [":six:",0,0,0,0,0,0,0,0],
-      [":seven:",0,0,0,0,0,0,0,0],
-      [":eight:",0,0,0,0,0,0,0,0]
+      [":one:",    0,0,0,0,0,0,0,0],
+      [":two:",    0,0,0,0,0,0,0,0],
+      [":three:",  0,0,0,0,0,0,0,0],
+      [":four:",   0,0,0,1,2,0,0,0],
+      [":five:",   0,0,0,2,1,0,0,0],
+      [":six:",    0,0,0,0,0,0,0,0],
+      [":seven:",  0,0,0,0,0,0,0,0],
+      [":eight:",  0,0,0,0,0,0,0,0]
     ] 
   json.push({id : message.channel.id, board : playBoard , flg:Math.floor(Math.random() * 2 + 1)})
   const ban = json.value().flg.replace(/2/ , "黒").replace(/1/ , "白")
@@ -41,8 +41,8 @@ module.exports.run = (client , message , db , args) => {
     const playBoard = json.value().board
     if(playBoard[args[0][args[1]]] !==　0) return message.channel.send("そこはすでに配置されています。")
     playBoard[args[0]][args[1]] === json.value().flg
-  json.assigin({board : playBoard})
-
+  json.assign({board : playBoard})
+kakunin(playBoard)
   if(json.value().flg === "1") {
     json.assign({flg : 2})
   var embed = new Discord.RichEmbed()
@@ -60,3 +60,32 @@ module.exports.run = (client , message , db , args) => {
   }
 }
 
+
+function kakunin(ban_ar , db , message){
+var check_reverse_cnt = 0
+  var white_cnt = 0
+var black_cnt = 0
+for (var x = 1; x < 9; x++) {
+for (var y = 1; y < 9; y++) {
+// 空白マスのみおけるのでチェック
+// それ以外は石の数を加算
+switch( ban_ar[x][y] ) {
+case 0:
+check_reverse_cnt = check_reverse_cnt + check_reverse(x,y)
+// バックアップから元に戻す
+for (var i = 1; i < 9; i++) {
+for (var ii = 1; ii < 9; ii++) {
+ban_ar[i][ii] = db.get("osero").find({ id :message.channel.id}).value().playBoard
+}
+}
+break;
+case -1:
+white_cnt++
+break
+case 1:
+black_cnt++
+break
+}
+}
+}
+}
