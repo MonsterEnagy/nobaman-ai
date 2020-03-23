b177bfa95bb590ce5acbf5929a89f71df9a3d65b
Glitch (nobaman-ai) <none>
2020-03-23 13:35:48 +0000

    💂🚝 Checkpoint ./command/hiragana.js:8778187/507

--- command/hiragana.js
+++ command/hiragana.js
@@ -1,3 +1,11 @@
 module.exports.run = (client , message , kekka) => {
-  require(`{`)
+  const parser = require("xml2json");
+  const request = require("request")
+  request({
+      url : `https://jlp.yahooapis.jp/JIMService/V1/conversion?appid=${process.env.yahooclientID}&sentence=${encodeURIComponent(kekka.trim())}`
+    } ,(res , body) => {
+    console.log(body.body)
+    const json = JSON.parse(parser.toJson(body))
+   console.log(json.Result)
+  })
 }
