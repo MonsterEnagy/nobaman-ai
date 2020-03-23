96c37d3003b0b5276692f988415087e977cd7b04
Glitch (nobaman-ai) <none>
2020-03-23 14:22:52 +0000

    🐄💶 Checkpoint ./command/hiragana.js:8778187/554 ./database/db.json:8778187/348 ./command/help.js:8778187/35

--- command/hiragana.js
+++ command/hiragana.js
@@ -4,6 +4,8 @@ module.exports.run = (client , message , kekka) => {
   request({
       url : `https://jlp.yahooapis.jp/JIMService/V1/conversion?appid=${process.env.yahooclientID}&sentence=${encodeURIComponent(kekka.trim())}`
     } ,(res , body) => {
+        console.log(body.body)
+    if(!JSON.parse(parser.toJson(body.body)).ResultSet) return message.channel.send("エラー" + JSON.stringify(body.body))    
     const json = JSON.parse(parser.toJson(body.body)).ResultSet.Result.SegmentList.Segment
     const result = [];
     const rei = [];
@@ -11,10 +13,11 @@ module.exports.run = (client , message , kekka) => {
     console.dir(json[i].CandidateList["Candidate"])
       if(typeof json[i].CandidateList["Candidate"] === "string") {
         json[i].CandidateList["Candidate"] = [json[i].CandidateList["Candidate"]]
-      } else if()
+      } 
+      
      result.push(`${json[i].SegmentText} => ${json[i].CandidateList["Candidate"].join(",")}\n`)
-     rei.push()
+     rei.push(json[i].CandidateList["Candidate"][0])
    }
-   message.channel.send(`\`${result.join("` `")}\`\n`)
+   message.channel.send(`\`${result.join("` `")}\`\n\`例文：${rei.join("")}\``)
   })
 }
