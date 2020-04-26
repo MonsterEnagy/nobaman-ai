ae19a1ae3dd7c3d2f4fdc924e2060d94015277b6
Glitch (nobaman-ai) <none>
2020-04-26 00:31:56 +0000

    🛬👡 Checkpoint ./command/icon.js:8778187/1335

--- command/icon.js
+++ command/icon.js
@@ -1,34 +1,5 @@
 module.exports.run = (client , message, args) => {
- /* const Discord = require("discord.js")
-let user;
-const id = client.users.get(args[0])
-const name = client.users.find(m=>m.username === args.join(" "));
-const mention = message.mentions.users.first()
-console.log("通ってる")
- if(!args[0] && !message.mentions.users) {
-user = client.users.get(message.author.id);
-} else if(message.mentions.user) {
-  user = mention
- } else if(name) {
-   user = name
-  } else if(id) {
-    user = id 
-    } else {
-    var userCollection = message.guild.members.filter(m=> m.user.username.toLowerCase().startsWith(args.join(" ").toLowerCase()))
- if(userCollection.size === 0) { return user = client.users.get(message.author.id) }
-    var username = userCollection.first().user.username //引数がKoだとしたら -> Kotlinモンスター
-    user = message.guild.members.find(m=>m.user.username === username).user
-}
- if(user.avatarURL === null) {
-  let embed = new Discord.RichEmbed()
-  .setAuthor(`${user.username}`)
-  .setImage(user.displayAvatarURL)
-  message.channel.send(embed)
-  } else {
-  let embed = new Discord.RichEmbed()
-  .setAuthor(`${user.username}`)
-  .setImage(user.avatarURL)
-  message.channel.send(embed)
- } */
-  message.channel.send("開発中")
+const Discord = require("discord.js")
+const collection = client.users.get(args[0]) || message.mentions.users.first() || message.guild.users.find(m=>m.startsWith(args[0]))
+
 }
