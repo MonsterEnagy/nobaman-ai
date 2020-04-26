8a2cc573bb4a2c86a8ed52cce7194723358dd6d0
Glitch (nobaman-ai) <none>
2020-04-26 00:55:47 +0000

    🎣🖼 Checkpoint ./command/icon.js:8778187/6

--- command/icon.js
+++ command/icon.js
@@ -1,6 +1,6 @@
 module.exports.run = (client , message, args) => {
 const Discord = require("discord.js")
-const collection = client.users.get(args[0]) || message.mentions.users.first() || message.guild.members.find(m=>m.user.username.toLowerCase.startsWith(args[0])) || message.author
+const collection = client.users.get(args[0]) || message.mentions.users.first() || message.guild.members.find(m=>m.user.username.toLowerCase().startsWith(args[0])) || message.author
 let embed = new Discord.RichEmbed()
 .setTitle(collection.tag ||  collection.user.tag)
 .setImage(collection.avatarURL || collection.user.avatarURL)
