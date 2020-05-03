381533c1e48c3cf0dd2aed441dbe083fca9503ec
Glitch (nobaman-ai) <none>
2020-05-03 06:58:03 +0000

    📅🎇 Checkpoint ./command/switch.js:8778187/56

--- command/switch.js
+++ command/switch.js
@@ -2,6 +2,7 @@ const Discord = require("discord.js")
 module.exports.run = (client , message, args , db) => {
 const emoji = [];
 for(var i = 0; i < message.guild.channels.size; i++){
+  i.replace(0 , "")
   message.channels.first(i)
 }
 message.channel.send(new Discord.RichEmbed()
