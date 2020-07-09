191ec29d96dfd26b2349f63bb0f72f3cb4fa9dc8
MonsterEnagy <57344208+MonsterEnagy@users.noreply.github.com>
2020-07-09 21:14:17 +0900

    express

--- nobaman.js
+++ nobaman.js
@@ -1152,6 +1152,17 @@ point : "-999"
   }
 });
 
+const express = require('express')
+const path = require('path')
+const PORT = process.env.PORT || 5000
+
+express()
+  .use(express.static(path.join(__dirname, 'public')))
+  .set('views', path.join(__dirname, 'views'))
+  .set('view engine', 'ejs')
+  .get('/', (req, res) => res.render('pages/index'))
+  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+
 client.login(process.env.token);
 
 const express = require("express");
