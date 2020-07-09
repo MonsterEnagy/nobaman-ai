753e5f556cf9fdbce13d6980661f5a5bfecaa8a7
MonsterEnagy <57344208+MonsterEnagy@users.noreply.github.com>
2020-07-09 21:26:41 +0900

    Update nobaman.js

--- nobaman.js
+++ nobaman.js
@@ -1160,7 +1160,7 @@ express()
   .use(express.static(path.join(__dirname, 'public')))
   .set('views', path.join(__dirname, 'views'))
   .set('view engine', 'ejs')
-  .get('/', (req, res) => res.render('pages/index'))
+  .get('/', (req, res) => res.render('./views/pages/index.ejs'))
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
 
 client.login(process.env.token);
